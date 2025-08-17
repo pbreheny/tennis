68aa5ca5d71d65f31ef0484f298239b04576ae7d
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-08-17 12:04:36 -0500

    Fixing misspecfication of prior: each new player has own mean, sd now

--- R/import.r
+++ R/import.r
@@ -1,8 +1,8 @@
 #' Import data
-#' 
+#'
 #' @year Year (numeric)
 #' @side atp/wta (string)
-#' 
+#'
 #' @examples
 #' import(2017, "wta")
 
@@ -14,8 +14,11 @@ import <- function(year, side = c("atp", "wta")) {
     dat_list[[i]][, winner_sets := sets[1,]]
     dat_list[[i]][, loser_sets := sets[2,]]
   }
+  # Exclude walkovers and Davis Cup (there are just too many weird Davis Cup scenarios)
   dat <- rbindlist(dat_list) |>
-    _[score != "W/O"]
+    _[score != "W/O"] |>
+    _[score != "RET"] |>
+    _[tourney_level != "D"]
   dat[surface == "Carpet", surface := "Grass"]
 
   dat[, .(
@@ -37,7 +40,8 @@ parse_score <- function(score) {
   split_score <- score |>
     str_replace(" RET", "") |>
     str_replace(" DEF", "") |>
-    str_split_1(" ") |> 
+    str_replace(" Played and unfinished", "") |>
+    str_split_1(" ") |>
     purrr::map(str_split_1, pattern = "-")
   vapply(split_score, \(x) c(x[1] > x[2], x[2] > x[1]), numeric(2)) |>
     rowSums()
--- R/make_prior.r
+++ R/make_prior.r
@@ -1,24 +1,36 @@
+#' Read in last year's prior (or create new one)
+
 make_prior <- function(id, side, year) {
   last_year <- glue('posterior/{side}/{min(year)-1}.rds')
   if (file.exists(last_year)) {
-    posterior <- readRDS(last_year)
+    post <- readRDS(last_year)
+    current <- post$eta[, last(.SD), player]
+    ind <- match(id, current$player)
+    mu_eta <- current[ind]$mean
+    mu_eta[is.na(mu_eta)] <- -1
+    tau_eta <- current[ind]$sd^(-2)
+    tau_eta[is.na(tau_eta)] <- 1
+    mu_alpha <- dcast(post$alpha, player ~ surface, value.var = "mean") |>
+      _[ind,2:4] |>
+      as.matrix()
+    mu_alpha[is.na(mu_alpha)] <- 0
+    tau_alpha <- post$alpha[, precision := sd^(-2)] |>
+      dcast(player ~ surface, value.var = "precision") |>
+      _[ind,2:4] |>
+      as.matrix()
+    tau_alpha[is.na(tau_alpha)] <- 1
+    rss <- post$tau$rss
+    tdf <- post$tau$tdf
   } else {
-    eta <- matrix(NA, ncol = 3)
-    sd_eta <- matrix(NA, ncol = 3)
-    alpha <- matrix(NA, ncol = 3)
-    sd_alpha <- matrix(NA, ncol = 3)
-    rss <- rep(10, 3)
-    tdf <- rep(1, 3)
+    eta_mean <- NA
+    eta_prec <- NA
+    mu_alpha <- matrix(0, length(id), 3)
+    tau_alpha <- matrix(1, length(id), 3)
+    rss <- rep(10, 5)
+    tdf <- rep(1, 5)
   }
-  ind <- match(id, rownames(eta))
-  new <- is.na(ind)
-  mu_eta <- eta[ind, ncol(eta)]
-  tau_eta <- sd_eta[ind, ncol(eta)]^(-2)
-  mu_alpha <- alpha[ind, ]
-  tau_alpha <- sd_alpha[ind, ]^(-2)
-  mu_eta[new] <- tau_eta[new] <- 0
-  mu_alpha[new,] <- tau_alpha[new,] <- 0
-
-  list(mu_eta = mu_eta, tau_eta = tau_eta, mu_alpha = mu_alpha, tau_alpha = tau_alpha,
-    rss = rss, tdf = tdf, new = 1 * new)
+  list(
+    mu_eta = mu_eta, tau_eta = tau_eta, mu_alpha = mu_alpha, tau_alpha = tau_alpha,
+    rss = rss, tdf = tdf
+  )
 }
